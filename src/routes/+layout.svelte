630957bf19b5b35c3f4d0296cac9564a10989b87
yikuansun <yikuansun2015@gmail.com>
2025-10-23 17:03:16 -0400

    Create layout n stufff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,4 +8,26 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<div class="header">
+	Hi
+</div>
+
 {@render children?.()}
+
+<style>
+	:global(body) {
+		position: relative;
+		margin: 0;
+	}
+
+	.header {
+		position: sticky;
+		top: 0;
+		z-index: 10;
+		width: 100%;
+		height: 72px;
+		background-color: hsla(0, 0%, 95%, 0.3);
+		box-shadow: 0 0 15px rgba(0, 0, 0, 0.3);
+		backdrop-filter: blur(12px);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div style:height="50vh" style:width="100%" style:display="flex" style:justify-content="center" style:align-items="center">
+    <div style:text-align="center">
+        <h1 style:font-size="81px" style:letter-spacing="20px" style:margin="0">AGORA</h1>
+        <p style:font-size="28px">The heart of the digital marketplace</p>
+    </div>
+</div>
+
+<div>
+    <h1>About</h1>
+    <p>
+        Teddy Zheng
+    </p>
+</div>
