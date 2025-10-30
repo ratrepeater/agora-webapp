e842b0b90f20ba8259258885343bc7d7a5ad51fa
yikuansun <yikuansun2015@gmail.com>
2025-10-30 10:01:22 -0400

    Clean up menubar
    ok

--- src/lib/components/MenuBar.svelte
+++ src/lib/components/MenuBar.svelte
@@ -1,29 +1,56 @@
 <div class="navbar bg-base-100/50 backdrop-blur-md shadow-md fixed">
-    <div class="flex-1">
+    <div class="navbar-start">
         <a href="/" class="btn btn-ghost text-xl">AGORA</a>
+        <details class="dropdown">
+            <summary class="btn btn-ghost">Departments</summary>
+            <ul class="menu xl:menu-horizontal bg-base-200 rounded-box lg:min-w-max fixed shadow-sm">
+                <li>
+                    <a>Industries</a>
+                    <ul>
+                    <li><a>Design</a></li>
+                    <li><a>Development</a></li>
+                    <li><a>Hosting</a></li>
+                    <li><a>Domain register</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a>Categories</a>
+                    <ul>
+                    <li><a>CRM software</a></li>
+                    <li><a>Marketing management</a></li>
+                    <li><a>Security</a></li>
+                    <li><a>Consulting</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </details>
     </div>
-    <div class="flex gap-2">
-    <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
-    <div class="dropdown dropdown-end">
-        <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
-            <div class="w-10 rounded-full">
-            <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-            </div>
-        </div>
-        <ul
-            tabindex="-1"
-            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-            <li>
-            <a class="justify-between">
-                Profile
-                <span class="badge">New</span>
-            </a>
-            </li>
-            <li><a>Settings</a></li>
-            <li><a>Logout</a></li>
-        </ul>
-        </div>
+    <div class="navbar-center">
+        <label class="input w-lg">
+            <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <g
+                stroke-linejoin="round"
+                stroke-linecap="round"
+                stroke-width="2.5"
+                fill="none"
+                stroke="currentColor"
+                >
+                <circle cx="11" cy="11" r="8"></circle>
+                <path d="m21 21-4.3-4.3"></path>
+                </g>
+            </svg>
+            <input type="search" class="grow" placeholder="Search software by name, category, or industry" />
+        </label>
+    </div>
+    <div class="navbar-end">
+        <button class="btn btn-ghost">
+            Request Invite
+        </button>
+        <button class="btn ml-3">
+            List Your Business
+        </button>
+        <button class="btn btn-primary ml-3">
+            Investor Portal
+        </button>
     </div>
 </div>
