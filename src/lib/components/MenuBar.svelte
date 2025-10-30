d1f9cef9e5357e42ffe649d0b2f4fa0d5a53ced0
yikuansun <yikuansun2015@gmail.com>
2025-10-29 23:10:34 -0400

    Switch to tailwind
    Pretty nice

--- src/lib/components/MenuBar.svelte
+++ src/lib/components/MenuBar.svelte
@@ -1,39 +1,29 @@
-<div class="header">
-    <div style:width="50%" style:margin-left="50px">
-        <h3 class="header-title">
-            <a href="/" class="header-link">AGORA</a>
-        </h3>
+<div class="navbar bg-base-100 shadow-sm fixed">
+    <div class="flex-1">
+        <a href="/" class="btn btn-ghost text-xl">AGORA</a>
     </div>
-    <div style:width="50%" style:text-align="right" style:margin-right="50px">
-        <a class="header-link" href="/about">About</a>
-        <a class="header-link" href="/pricing">Pricing</a>
-        <a class="header-link" href="/businesses">Businesses</a>
+    <div class="flex gap-2">
+    <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
+    <div class="dropdown dropdown-end">
+        <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+            <div class="w-10 rounded-full">
+            <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            </div>
+        </div>
+        <ul
+            tabindex="-1"
+            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            <li>
+            <a class="justify-between">
+                Profile
+                <span class="badge">New</span>
+            </a>
+            </li>
+            <li><a>Settings</a></li>
+            <li><a>Logout</a></li>
+        </ul>
+        </div>
     </div>
 </div>
-
-
-<style>
-	.header {
-		position: sticky;
-		top: 0;
-		z-index: 10;
-		width: 100%;
-		height: 72px;
-		background-color: hsla(0, 0%, 95%, 0.3);
-		box-shadow: 0 0 15px rgba(0, 0, 0, 0.3);
-		backdrop-filter: blur(12px);
-        display: flex;
-        align-items: center;
-        justify-content: center;
-	}
-
-    .header-title {
-        letter-spacing: 5px;
-    }
-
-    .header-link {
-        margin: 0 20px;
-        text-decoration: none;
-        color: black
-    }
-</style>
