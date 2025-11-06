49d666f3c92eb869c43d3f7c73209b6d20a443bc
yikuansun <yikuansun2015@gmail.com>
2025-11-06 17:55:53 -0500

    Get listings from server

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,24 @@
     import { goto } from '$app/navigation';
     import InfoSection from '$lib/components/InfoSection.svelte';
     import ListingCard from '$lib/components/ListingCard.svelte';
+	import { onMount } from 'svelte';
+    import type { ListingInfo } from '$lib/helpers/types';
+
+
+    let listings : ListingInfo[] = [];
+
+    onMount(() => {
+        // TODO: abstract away API calls
+        fetch('/api/listings')
+            .then(res => res.json())
+            .then(data => {
+                listings = data;
+            });
+    });
 </script>
 
 <InfoSection>
-    <ListingCard data={{
-        uid: "123",
-        created_at: "2023-07-01T00:00:00.000Z",
-        creator_id: "123",
-        title: "Agora",
-        description: "Agora is a decentralized platform for software developers to build and monetize their own software products.",
-        tags: ["Agora", "Decentralized", "Software", "Developer", "Product", "Monetization"],
-        public: true
-    }}/>
+    {#each listings as data}
+        <ListingCard data={data} />
+    {/each}
 </InfoSection>
---
+++ src/routes/api/listings/+server.js
@@ -0,0 +1,13 @@
+import { supabase } from '$lib/helpers/supabase';
+
+export async function GET() {
+    let { data: listings, error } = await supabase
+        .from('listings')
+        .select('*');
+          
+    if (error) {
+        return { status: 500, body: error.message };
+    }
+
+    return new Response(JSON.stringify(listings), { status: 200, headers: { 'Content-Type': 'application/json' } });
+}
