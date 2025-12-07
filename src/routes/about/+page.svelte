ed5e929296c885219834e29e6c7905a5bd9473e0
Teddy Zheng <teddyzheng@college.harvard.edu>
2025-12-07 14:26:22 -0500

    deleted unnecessary files

--- src/routes/about/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import { goto } from '$app/navigation';
-    import PageBanner from '$lib/components/PageBanner.svelte';
-    import InfoSection from '$lib/components/InfoSection.svelte';
-</script>
-
-<PageBanner height="50vh">
-    <h1 style:font-size="72px" style:margin="0">About</h1>
-</PageBanner>
