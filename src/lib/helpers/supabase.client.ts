9a492c4a1e82fddb6b7d97afdb0abba7ebac9a07
yikuansun <yikuansun2015@gmail.com>
2025-12-28 20:48:48 -0500

    Oosp
    Oosp

--- src/lib/helpers/supabase.client.ts
+++ src/lib/helpers/supabase.client.ts
@@ -1,6 +1,6 @@
 // client-side supabase singleton
 
 import { createBrowserClient } from './supabase';
-import { SUPABASE_URL, SUPABASE_KEY } from '$env/static/private';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
-export const supabase = createBrowserClient(SUPABASE_URL, SUPABASE_KEY);
+export const supabase = createBrowserClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
